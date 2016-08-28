c53302e641f0c6c174c58d09fc90d348001fc252
Eddy Petrișor <eddy.petrisor@gmail.com>
2016-08-28 23:24:55 +0300

    tests for rlistdir
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- tests/test_cc2gitutils.py
+++ tests/test_cc2gitutils.py
@@ -32,3 +32,62 @@ def test_insert_after_all_small_list():
 def test_insert_after_all_large_list():
 	l = insert_in_sorted_list(4, [0, 1, 3])
 	assert l == [0, 1, 3, 4]
+
+
+# rlistdir
+def test_rlistdir_no_files(tmpdir):
+	d = tmpdir.mkdir('a')
+	li = 'a/'
+
+	files = rlistdir(li, just_files=True)
+
+	assert files == []
+
+def test_rlistdir_one_subdir_file(tmpdir):
+
+	d = tmpdir.mkdir('a')
+	d = tmpdir.mkdir('a/b')
+	f = d.join('c.txt')
+	f.write('test')
+	li = 'a/'
+
+	oldd = tmpdir.chdir()
+	files = rlistdir(li, just_files=True)
+	f_and_d = rlistdir(li)
+	oldd.chdir()
+
+	assert files == ['a/b/c.txt']
+	assert f_and_d == [ 'a', 'a/b', 'a/b/c.txt']
+
+def test_rlistdir_one_file(tmpdir):
+
+	f = tmpdir.mkdir('a').join('b.txt')
+	f.write('test')
+	li = 'a/'
+
+	oldd = tmpdir.chdir()
+	files = rlistdir(li, just_files=True)
+	f_and_d = rlistdir(li)
+	oldd.chdir()
+
+	assert files == ['a/b.txt']
+	assert f_and_d == [ 'a', 'a/b.txt']
+
+def test_rlistdir_3_files(tmpdir):
+
+	d = tmpdir.mkdir('a')
+	f = d.join('b.txt')
+	f.write('test')
+	f = d.join('c.txt')
+	f.write('test')
+	f = d.join('d.txt')
+	f.write('test')
+	li = 'a/'
+
+	oldd = tmpdir.chdir()
+	files = rlistdir(li, just_files=True)
+	f_and_d = rlistdir(li)
+	oldd.chdir()
+
+	assert files == ['a/b.txt', 'a/c.txt', 'a/d.txt']
+	assert f_and_d == [ 'a', 'a/b.txt', 'a/c.txt', 'a/d.txt']
